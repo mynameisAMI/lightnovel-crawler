2ed023fa74a919d0dc09966d0461a2648d627739
amine bouhtani <133370975+mynameisAMI@users.noreply.github.com>
2025-02-04 01:30:25 +0100

    Update kolnovel.py

--- sources/ar/kolnovel.py
+++ sources/ar/kolnovel.py
@@ -8,4 +8,4 @@ logger = logging.getLogger(__name__)
 class Kolnovel(MangaStreamTemplate):
     has_mtl = False
     has_manga = False
-    base_url = ["https://kolnovel.com/"]
+    base_url = ["https://kolnovel.org/"]
